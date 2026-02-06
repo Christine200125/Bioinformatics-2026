8fd55e51abf788b6d3fbe64b7a326d930f1358d2
Christine200125 <christine.gouws@wmich.edu>
2026-02-05 19:48:16 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Add your commands below!
+greeting=Howzit
+
+your_name=Christine?
+
+echo "$greeting $your_name"
+
+echo "This script is helping you learn bash."
+
+mkdir /home/scooby/Desktop/bioinformatics/
+
+mkdir /home/scooby/Desktop/bioinformatics/bash_scripts
+
+mv /home/scooby/Desktop/bash_template.txt /home/scooby/Desktop/bioinformatics/bash_scripts
+
+mv /home/scooby/Desktop/bioinformatics/bash_scripts/bash_template.txt /home/scooby/Desktop/bioinformatics/bash_scripts/bash_template.sh 
